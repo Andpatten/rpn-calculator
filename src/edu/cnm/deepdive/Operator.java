34d875b55c4b99a721d7361d7e92d19544c4d410
Andrew Patten <andpatten@gmail.com>
2019-09-18 16:51:12 -0600

    Added Calculator and stored our inputs in a txt file.

--- src/edu/cnm/deepdive/Operator.java
+++ src/edu/cnm/deepdive/Operator.java
@@ -8,34 +8,41 @@ import java.util.Arrays;
  * output string.
  */
 public enum Operator {
-  /** Pops 2 values from stack, pushes sum of the 2 back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes sum of the 2 back onto stack.
+   */
   ADD("+"),
-  /** pops 2 values from stack, pushes differences of the 2 back onto stack. */
+  /**
+   * pops 2 values from stack, pushes differences of the 2 back onto stack.
+   */
   SUBTRACT("-"),
-  /** Pops 2 values from stack, pushes product of the two back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes product of the two back onto stack.
+   */
   MULTIPLY("*"),
-  /** Pops 2 values from stack, pushes quotient of the two back onto stack. */
+  /**
+   * Pops 2 values from stack, pushes quotient of the two back onto stack.
+   */
   DIVIDE("/"),
-  /** Pops 1 value from the stack, pushes the square root of the two back onto stack. */
+  /**
+   * Pops 1 value from the stack, pushes the square root of the two back onto stack.
+   */
   SQUARE_ROOT("sqrt"),
-  /** Pops 2 values from the stack, pushes the value of 1st raised to the 2nd back onto stack. */
+  /**
+   * Pops 2 values from the stack, pushes the value of 1st raised to the 2nd back onto stack.
+   */
   POWER("^"),
-  /** Pops two values from stakc, pushes remainder after truncated division of the two back onto stack. */
+  /**
+   * Pops two values from stakc, pushes remainder after truncated division of the two back onto
+   * stack.
+   */
   MODULO("%");
 
   private String token;
 
   Operator(String token) {
     this.token = token;
-  }
 
-  /**
-   * Exercises the Operator enum by printing all of the enumerated values (as an array).
-   *
-   * @param args
-   */
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(Operator.values()));
   }
 
   @Override
@@ -43,5 +50,8 @@ public enum Operator {
     return token;
   }
 
+  public static String tokenPattern() {
+    return "(?:^|\\s)(\\+|\\-|\\*|\\/|\\^|\\%|SQUARE_ROOT)(?:\\s|$)";
+  }
   //TODO Add operate method with switch (later version will use @override).
 }
